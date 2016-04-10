614962361f7f4fb7e7c66d340d942d90ba5675bf
zhangxufan <sunshinezxf@hotmail.com>
2016-04-10 23:37:08 +0800

    add dashboard handling

---
+++ src/main/java/selling/sunshine/controller/PlatformController.java
@@ -0,0 +1,51 @@
+package selling.sunshine.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by sunshine on 4/10/16.
+ */
+@RequestMapping("/")
+@RestController
+public class PlatformController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public ModelAndView index() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("redirect:/login");
+        return view;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/login")
+    public ModelAndView login() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("/backend/login");
+        return view;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/login")
+    public ModelAndView login(HttpServletRequest request) {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("redirect:/dashboard");
+        return view;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/dashboard")
+    public ModelAndView dashboard() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("/backend/dashboard");
+        return view;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/logout")
+    public ModelAndView logout() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("redirect:/login");
+        return view;
+    }
+}
