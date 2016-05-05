a0ea307e0fb913b1f0d1566a6846eb29ac43008b
qinzhenning <1026529451@qq.com>
2016-05-05 11:42:10 +0800

    customer.java修改

--- src/main/java/selling/sunshine/model/Customer.java
+++ src/main/java/selling/sunshine/model/Customer.java
@@ -4,41 +4,53 @@ package selling.sunshine.model;
  * Created by sunshine on 4/8/16.
  */
 public class Customer extends Entity {
-    private String customerId;
-    private String name;
-    private String address;
+	private String customerId;
+	private String name;
+	private String address;
+	private String phone;
 
-    public Customer() {
-        super();
-    }
+	public Customer() {
+		super();
+	}
 
-    public Customer(String name, String address) {
-        this();
-        this.name = name;
-        this.address = address;
-    }
+	public Customer(String customerId, String name, String address, String phone) {
+		super();
+		this.customerId = customerId;
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+	}
 
-    public String getCustomerId() {
-        return customerId;
-    }
+	public String getCustomerId() {
+		return customerId;
+	}
 
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
 }
