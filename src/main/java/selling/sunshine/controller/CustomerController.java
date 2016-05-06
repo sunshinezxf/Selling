a47fc644ba6c85359c51dab67c2f3081d7e9415d
qinzhenning <1026529451@qq.com>
2016-05-06 16:07:08 +0800

    代理商添加客户

--- src/main/java/selling/sunshine/controller/CustomerController.java
+++ src/main/java/selling/sunshine/controller/CustomerController.java
@@ -1,10 +1,24 @@
 package selling.sunshine.controller;
 
+import javax.validation.Valid;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import selling.sunshine.form.CustomerForm;
+import selling.sunshine.model.Agent;
+import selling.sunshine.model.Customer;
+import selling.sunshine.model.User;
+import selling.sunshine.service.CustomerService;
+import selling.sunshine.utils.ResponseCode;
 import selling.sunshine.utils.ResultData;
 
 /**
@@ -14,26 +28,46 @@ import selling.sunshine.utils.ResultData;
 @RestController
 public class CustomerController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/overview")
-    public ModelAndView overview() {
-        ModelAndView view = new ModelAndView();
-        view.setViewName("/backend/customer/overview");
-        return view;
-    }
+	@Autowired
+	private CustomerService customerService;
 
-    @ResponseBody
-    @RequestMapping(method = RequestMethod.POST, value = "/add")
-    public ResultData addCustomer() {
-        ResultData result = new ResultData();
+	@RequestMapping(method = RequestMethod.GET, value = "/overview")
+	public ModelAndView overview() {
+		ModelAndView view = new ModelAndView();
+		view.setViewName("/backend/customer/overview");
+		return view;
+	}
 
-        return result;
-    }
+	@ResponseBody
+	@RequestMapping(method = RequestMethod.POST, value = "/add")
+	public ResultData addCustomer(@Valid CustomerForm customerForm,
+			BindingResult result) {
+		System.out
+				.println("ceshi----------------------------------------------------------------------------");
+		ResultData resultData = new ResultData();
+		if (result.hasErrors()) {
+			resultData.setResponseCode(ResponseCode.RESPONSE_ERROR);
+			return resultData;
+		}
+		Subject subject = SecurityUtils.getSubject();
+		User user = null;
+		Agent agent = null;
+		if (subject != null) {
+			Session session = subject.getSession();
+			user = (User) session.getAttribute("current");
+			agent = user.getAgent();
+		}
+		Customer customer = new Customer(customerForm.getName(),
+				customerForm.getAddress(), customerForm.getPhone(), agent);
+		resultData = customerService.createCustomer(customer);
+		return resultData;
+	}
 
-    @ResponseBody
-    @RequestMapping(method = RequestMethod.POST, value = "/modify")
-    public ResultData updateCustomer() {
-        ResultData result = new ResultData();
+	@ResponseBody
+	@RequestMapping(method = RequestMethod.POST, value = "/modify")
+	public ResultData updateCustomer() {
+		ResultData result = new ResultData();
 
-        return result;
-    }
+		return result;
+	}
 }
