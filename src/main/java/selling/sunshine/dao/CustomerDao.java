9549d169eba64e8256cf6f7ed66c949d82ca3ff7
zhangxufan <sunshinezxf@hotmail.com>
2016-05-06 13:52:00 +0800

    some changes

--- src/main/java/selling/sunshine/dao/CustomerDao.java
+++ src/main/java/selling/sunshine/dao/CustomerDao.java
@@ -1,10 +1,9 @@
 package selling.sunshine.dao;
 
-import java.util.Map;
-
-import selling.sunshine.model.Agent;
 import selling.sunshine.model.Customer;
 import selling.sunshine.utils.ResultData;
+
+import java.util.Map;
 
 /**
  * Created by sunshine on 5/5/16.
@@ -13,6 +12,6 @@ public interface CustomerDao {
     ResultData insertCustomer(Customer customer);
 
     ResultData updateCustomer(Customer customer);
-    
+
     ResultData queryCustomer(Map<String, Object> condition);
 }
--- src/main/java/selling/sunshine/dao/impl/CustomerDaoImpl.java
+++ src/main/java/selling/sunshine/dao/impl/CustomerDaoImpl.java
@@ -1,16 +1,10 @@
 package selling.sunshine.dao.impl;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
-
 import selling.sunshine.dao.BaseDao;
 import selling.sunshine.dao.CustomerDao;
-import selling.sunshine.model.Agent;
 import selling.sunshine.model.Customer;
 import selling.sunshine.model.CustomerAddress;
 import selling.sunshine.model.CustomerPhone;
@@ -19,78 +13,82 @@ import selling.sunshine.utils.IDGenerator;
 import selling.sunshine.utils.ResponseCode;
 import selling.sunshine.utils.ResultData;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by sunshine on 5/5/16.
  */
 @Repository
 public class CustomerDaoImpl extends BaseDao implements CustomerDao {
 
-	private Logger logger = LoggerFactory.getLogger(CustomerDaoImpl.class);
-	private Object lock = new Object();
-	
-	@Override
-	public ResultData insertCustomer(Customer customer) {
-		ResultData result = new ResultData();
-		 synchronized (lock) {
-	            try {
-	                customer.setCustomerId(IDGenerator.generate("CUS"));
-	                sqlSession.insert("selling.customer.insert", customer);
-	                CustomerPhone phoneNumber=customer.getPhone();
-	                CustomerAddress address=customer.getAddress();
-	                phoneNumber.setPhoneId(IDGenerator.generate("PNM"));
-	                address.setAddressId(IDGenerator.generate("ADD"));
-	                sqlSession.insert("selling.customer.phone.insert", phoneNumber);
-	                sqlSession.insert("selling.customer.address.insert", address);
-	                result.setData(customer);
-	            } catch (Exception e) {
-	                logger.error(e.getMessage());
-	                result.setResponseCode(ResponseCode.RESPONSE_ERROR);
-	                result = insertCustomer(customer);
-	            } finally {
-	                return result;
-	            }
-	        }
-	}
+    private Logger logger = LoggerFactory.getLogger(CustomerDaoImpl.class);
+    private Object lock = new Object();
 
-	@Override
-	/*
-	 * 在更新一个customer之前要先根据条件查询这个customer是否已存在,存在的话要获得这个customer的现有地址和电话，然后block
-	 */
-	public ResultData updateCustomer(Customer customer) {
-		ResultData result = new ResultData();
-		try {
-			
-            Map<String, Object> condition=new HashMap<String, Object>();
-            condition.put("customerId",customer.getCustomerId());
-            condition.put("name",customer.getName());
-            condition.put("blockFlag",customer.isBlockFlag());
-            
-			if(ResponseCode.RESPONSE_OK.equals(queryCustomer(condition).getResponseCode())){
-				CustomerPhone phoneNumber=customer.getPhone();
-                CustomerAddress address=customer.getAddress();
-                //添加customer现有地址和电话
+    @Override
+    public ResultData insertCustomer(Customer customer) {
+        ResultData result = new ResultData();
+        synchronized (lock) {
+            try {
+                customer.setCustomerId(IDGenerator.generate("CUS"));
+                sqlSession.insert("selling.customer.insert", customer);
+                CustomerPhone phoneNumber = customer.getPhone();
+                CustomerAddress address = customer.getAddress();
+                phoneNumber.setPhoneId(IDGenerator.generate("PNM"));
+                address.setAddressId(IDGenerator.generate("ADD"));
                 sqlSession.insert("selling.customer.phone.insert", phoneNumber);
                 sqlSession.insert("selling.customer.address.insert", address);
-                Customer c=((List<Customer>)queryCustomer(condition).getData()).get(0);
-                //把customer原有地址和电话block掉
-                sqlSession.update("selling.customer.phone.block",c.getPhone());
-                sqlSession.update("selling.customer.address.block",c.getAddress());                
-			}
-			result.setData(customer);
-			result.setResponseCode(ResponseCode.RESPONSE_OK);
-		} catch (Exception e) {
-			logger.error(e.getMessage());
-            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
-		}finally{
-			return result;
-		}	
-		
-	}
+                result.setData(customer);
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+                result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+                result = insertCustomer(customer);
+            } finally {
+                return result;
+            }
+        }
+    }
 
-	@Override
-	public ResultData queryCustomer(Map<String, Object> condition) {
-		ResultData result = new ResultData();
-		try {
+    @Override
+    /*
+     * 锟节革拷锟斤拷一锟斤拷customer之前要锟饺革拷锟斤拷锟斤拷锟斤拷锟斤拷询锟斤拷锟絚ustomer锟角凤拷锟窖达拷锟斤拷,锟斤拷锟节的伙拷要锟斤拷锟斤拷锟斤拷customer锟斤拷锟斤拷锟叫碉拷址锟酵电话锟斤拷然锟斤拷block
+	 */
+    public ResultData updateCustomer(Customer customer) {
+        ResultData result = new ResultData();
+        try {
+
+            Map<String, Object> condition = new HashMap<String, Object>();
+            condition.put("customerId", customer.getCustomerId());
+            condition.put("name", customer.getName());
+            condition.put("blockFlag", customer.isBlockFlag());
+
+            if (ResponseCode.RESPONSE_OK.equals(queryCustomer(condition).getResponseCode())) {
+                CustomerPhone phoneNumber = customer.getPhone();
+                CustomerAddress address = customer.getAddress();
+                //锟斤拷锟絚ustomer锟斤拷锟叫碉拷址锟酵电话
+                sqlSession.insert("selling.customer.phone.insert", phoneNumber);
+                sqlSession.insert("selling.customer.address.insert", address);
+                Customer c = ((List<Customer>) queryCustomer(condition).getData()).get(0);
+                //锟斤拷customer原锟叫碉拷址锟酵电话block锟斤拷
+                sqlSession.update("selling.customer.phone.block", c.getPhone());
+                sqlSession.update("selling.customer.address.block", c.getAddress());
+            }
+            result.setData(customer);
+            result.setResponseCode(ResponseCode.RESPONSE_OK);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+        } finally {
+            return result;
+        }
+
+    }
+
+    @Override
+    public ResultData queryCustomer(Map<String, Object> condition) {
+        ResultData result = new ResultData();
+        try {
             List<Goods> list = sqlSession.selectList("selling.customer.query", condition);
             result.setData(list);
             result.setResponseCode(ResponseCode.RESPONSE_OK);
@@ -101,5 +99,5 @@ public class CustomerDaoImpl extends BaseDao implements CustomerDao {
         } finally {
             return result;
         }
-	}
+    }
 }
