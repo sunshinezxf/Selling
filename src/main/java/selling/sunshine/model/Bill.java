16a092f05b41317cd3e4ec841d8bfc1ff2bf21f1
zhangxufan <sunshinezxf@hotmail.com>
2016-05-10 16:06:15 +0800

    add deposit bill model and page display

--- src/main/java/selling/sunshine/model/Bill.java
+++ src/main/java/selling/sunshine/model/Bill.java
@@ -3,5 +3,61 @@ package selling.sunshine.model;
 /**
  * Created by sunshine on 5/10/16.
  */
-public class Bill {
+public abstract class Bill extends Entity {
+    private String billId;
+    private String clientIp;
+    private double billAmount;
+    private BillStatus status;
+
+    public Bill() {
+        super();
+        status = BillStatus.NOT_PAYED;
+    }
+
+    public Bill(double billAmount) {
+        this();
+        this.billAmount = billAmount;
+    }
+
+    public Bill(double billAmount, String clientIp) {
+        this(billAmount);
+        this.clientIp = clientIp;
+    }
+
+    public Bill(double billAmount, String clientIp, BillStatus status) {
+        this(billAmount, clientIp);
+        this.status = status;
+    }
+
+    public String getBillId() {
+        return billId;
+    }
+
+    public void setBillId(String billId) {
+        this.billId = billId;
+    }
+
+    public String getClientIp() {
+        return clientIp;
+    }
+
+    public void setClientIp(String clientIp) {
+        this.clientIp = clientIp;
+    }
+
+    public double getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(double billAmount) {
+        this.billAmount = billAmount;
+    }
+
+    public BillStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BillStatus status) {
+        this.status = status;
+    }
 }
--- src/main/java/selling/sunshine/model/BillStatus.java
+++ src/main/java/selling/sunshine/model/BillStatus.java
@@ -4,4 +4,15 @@ package selling.sunshine.model;
  * Created by sunshine on 5/10/16.
  */
 public enum BillStatus {
+    NOT_PAYED(0), PAYED(1), REFUND(2);
+
+    private int code;
+
+    BillStatus(int status) {
+        this.code = status;
+    }
+
+    public int getCode() {
+        return code;
+    }
 }
