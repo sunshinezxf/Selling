9039c1dbfaeee151d604755963ffb8b311389987
zhangxufan <sunshinezxf@hotmail.com>
2016-05-10 11:50:16 +0800

    initailly add deposit method in account controller

--- src/main/java/selling/sunshine/controller/AccountController.java
+++ src/main/java/selling/sunshine/controller/AccountController.java
@@ -1,7 +1,9 @@
 package selling.sunshine.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by sunshine on 4/14/16.
@@ -10,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController {
 
-
+    @RequestMapping(method = RequestMethod.GET, value = "/deposit")
+    public ModelAndView deposit() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("/agent/account/recharge");
+        return view;
+    }
 }
