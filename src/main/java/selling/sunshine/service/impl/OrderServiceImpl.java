df582edfec0218bf4d1de4ba56e6a558472a62a8
zhangxufan <sunshinezxf@hotmail.com>
2016-05-12 13:37:44 +0800

    implmentation of fetch order

--- src/main/java/selling/sunshine/service/impl/OrderServiceImpl.java
+++ src/main/java/selling/sunshine/service/impl/OrderServiceImpl.java
@@ -1,44 +1,46 @@
 package selling.sunshine.service.impl;
 
-import java.util.Map;
-import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import selling.sunshine.dao.AgentDao;
 import selling.sunshine.dao.OrderDao;
-import selling.sunshine.dao.OrderItemDao;
 import selling.sunshine.model.Order;
-import selling.sunshine.model.OrderItem;
 import selling.sunshine.service.OrderService;
 import selling.sunshine.utils.ResponseCode;
 import selling.sunshine.utils.ResultData;
 
+import java.util.Map;
+
 public class OrderServiceImpl implements OrderService {
-	private Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
+    private Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
 
-	@Autowired
-	private OrderDao orderDao;
+    @Autowired
+    private OrderDao orderDao;
 
-	@Override
-	public ResultData placeOrder(Order order) {
-		ResultData result = new ResultData();
-		ResultData insertResponse = orderDao.insertOrder(order);
-		result.setResponseCode(insertResponse.getResponseCode());
-		if(insertResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
-			result.setData(insertResponse.getData());
-		}else {
-			result.setDescription(insertResponse.getDescription());
-		}
-		return result;
-	}
+    @Override
+    public ResultData placeOrder(Order order) {
+        ResultData result = new ResultData();
+        ResultData insertResponse = orderDao.insertOrder(order);
+        result.setResponseCode(insertResponse.getResponseCode());
+        if (insertResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            result.setData(insertResponse.getData());
+        } else {
+            result.setDescription(insertResponse.getDescription());
+        }
+        return result;
+    }
 
-	@Override
-	public ResultData fetchOrder(Map<String, Object> condition) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public ResultData fetchOrder(Map<String, Object> condition) {
+        ResultData result = new ResultData();
+        ResultData queryResponse = orderDao.queryOrder(condition);
+        result.setResponseCode(queryResponse.getResponseCode());
+        if (queryResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            result.setData(queryResponse.getData());
+        } else {
+            result.setDescription(queryResponse.getDescription());
+        }
+        return result;
+    }
 
 }
