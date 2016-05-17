6c0489cf178448662279851238b666060845e038
zhangxufan <sunshinezxf@hotmail.com>
2016-05-17 16:15:25 +0800

    implementation of refund config

--- src/main/java/selling/sunshine/dao/RefundDao.java
+++ src/main/java/selling/sunshine/dao/RefundDao.java
@@ -1,7 +1,15 @@
 package selling.sunshine.dao;
 
+import selling.sunshine.model.RefundConfig;
+import selling.sunshine.utils.ResultData;
+
+import java.util.Map;
+
 /**
  * Created by sunshine on 5/17/16.
  */
-public class RefundDao {
+public interface RefundDao {
+    ResultData insertRefundConfig(RefundConfig config);
+
+    ResultData queryRefundConfig(Map<String, Object> condition);
 }
--- src/main/java/selling/sunshine/service/RefundService.java
+++ src/main/java/selling/sunshine/service/RefundService.java
@@ -1,7 +1,15 @@
 package selling.sunshine.service;
 
+import selling.sunshine.model.RefundConfig;
+import selling.sunshine.utils.ResultData;
+
+import java.util.Map;
+
 /**
  * Created by sunshine on 5/17/16.
  */
 public interface RefundService {
+    ResultData createRefundConfig(RefundConfig config);
+
+    ResultData fetchRefundConfig(Map<String, Object> condition);
 }
