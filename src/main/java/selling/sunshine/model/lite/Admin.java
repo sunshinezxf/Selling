a693cd34369e889b293c62610c31ced9482e2b26
zhangxufan <sunshinezxf@hotmail.com>
2016-05-20 16:43:04 +0800

    add lite model

---
+++ src/main/java/selling/sunshine/model/lite/Admin.java
@@ -0,0 +1,25 @@
+package selling.sunshine.model.lite;
+
+/**
+ * Created by sunshine on 5/20/16.
+ */
+public class Admin {
+    private String id;
+    private String username;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/selling/sunshine/model/lite/Agent.java
@@ -0,0 +1,34 @@
+package selling.sunshine.model.lite;
+
+/**
+ * Created by sunshine on 5/20/16.
+ */
+public class Agent {
+    private String id;
+    private String name;
+    private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
