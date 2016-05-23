2dd0a380e20584a7645105ce6514e2b2c2d1bb39
zhangxufan <sunshinezxf@hotmail.com>
2016-05-23 15:31:28 +0800

    refactor deposit bill and corresponding batis implementation

--- src/main/java/selling/sunshine/model/DepositBill.java
+++ src/main/java/selling/sunshine/model/DepositBill.java
@@ -1,31 +1,33 @@
 package selling.sunshine.model;
 
+import selling.sunshine.model.lite.Agent;
+
 /**
  * Created by sunshine on 5/10/16.
  */
 public class DepositBill extends Bill {
-    private selling.sunshine.model.lite.Agent agent;
+    private Agent agent;
 
     public DepositBill() {
         super();
-        agent = new selling.sunshine.model.lite.Agent();
+        agent = new Agent();
     }
 
-    public DepositBill(double billAmount, String channel, String clientIp, selling.sunshine.model.lite.Agent agent) {
+    public DepositBill(double billAmount, String channel, String clientIp, Agent agent) {
         super(billAmount, channel, clientIp);
         this.agent = agent;
     }
 
-    public DepositBill(double billAmount, String channel, String clientIp, BillStatus status, selling.sunshine.model.lite.Agent agent) {
+    public DepositBill(double billAmount, String channel, String clientIp, BillStatus status, Agent agent) {
         super(billAmount, channel, clientIp, status);
         this.agent = agent;
     }
 
-    public selling.sunshine.model.lite.Agent getAgent() {
+    public Agent getAgent() {
         return agent;
     }
 
-    public void setAgent(selling.sunshine.model.lite.Agent agent) {
+    public void setAgent(Agent agent) {
         this.agent = agent;
     }
 }
