9605a9df6f0f5133cb89f70542f4862c23ab2b75
unknown <>
2016-05-31 15:47:35 +0800

    1

---
+++ src/main/java/selling/sunshine/form/PasswordForm.java
@@ -0,0 +1,21 @@
+package selling.sunshine.form;
+
+public class PasswordForm {
+	private String password;
+	private String password2;
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPassword2() {
+		return password2;
+	}
+	public void setPassword2(String password2) {
+		this.password2 = password2;
+	}
+	
+	
+}
