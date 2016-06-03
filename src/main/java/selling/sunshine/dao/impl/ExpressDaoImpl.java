7526ff2c062957529cbd577fe91c19afec4b2261
zhangxufan <sunshinezxf@hotmail.com>
2016-06-03 13:29:08 +0800

    optimize express dao

--- src/main/java/selling/sunshine/dao/impl/ExpressDaoImpl.java
+++ src/main/java/selling/sunshine/dao/impl/ExpressDaoImpl.java
@@ -1,13 +1,9 @@
 package selling.sunshine.dao.impl;
 
-import java.util.List;
-
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import selling.sunshine.dao.BaseDao;
 import selling.sunshine.dao.ExpressDao;
 import selling.sunshine.model.Express;
@@ -15,45 +11,60 @@ import selling.sunshine.utils.IDGenerator;
 import selling.sunshine.utils.ResponseCode;
 import selling.sunshine.utils.ResultData;
 
-public class ExpressDaoImpl extends BaseDao implements ExpressDao {
+import java.util.List;
+import java.util.Map;
 
+@Repository
+public class ExpressDaoImpl extends BaseDao implements ExpressDao {
     private Logger logger = LoggerFactory.getLogger(ExpressDaoImpl.class);
 
     private Object lock = new Object();
 
+    /**
+     * 添加快递单记录
+     *
+     * @param express
+     * @return
+     */
     @Transactional
     @Override
-	public ResultData insertExpress(Express express) {
-		
-    	 ResultData result = new ResultData();
-         synchronized (lock) {
-             try {
-                 express.setExpressId(IDGenerator.generate("EXP"));
-                 sqlSession.insert("selling.express.insert", express);
-                 result.setData(express);
-             } catch (Exception e) {
-                 logger.error(e.getMessage());
-                 result.setResponseCode(ResponseCode.RESPONSE_ERROR);
-                 result = insertExpress(express);
-             } finally {
-                 return result;
-             }
-         }
-	}
+    public ResultData insertExpress(Express express) {
+        ResultData result = new ResultData();
+        synchronized (lock) {
+            try {
+                express.setExpressId(IDGenerator.generate("EXP"));
+                sqlSession.insert("selling.express.insert", express);
+                result.setData(express);
+                sqlSession.commit();
+            } catch (Exception e) {
+                sqlSession.rollback();
+                result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+                logger.error(e.getMessage());
+                result = insertExpress(express);
+            } finally {
+                return result;
+            }
+        }
+    }
 
-	@Override
-	public ResultData queryExpress(Map<String, Object> condition) {
-		 ResultData result = new ResultData();
-	        try {
-	            List<Express> list = sqlSession.selectList("selling.express.query",condition);
-	            result.setData(list);
-	        } catch (Exception e) {
-	            logger.error(e.getMessage());
-	            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
-	            result.setDescription(e.getMessage());
-	        } finally {
-	            return result;
-	        }
-	}
-
+    /**
+     * 根据条件查询快递列表
+     *
+     * @param condition
+     * @return
+     */
+    @Override
+    public ResultData queryExpress(Map<String, Object> condition) {
+        ResultData result = new ResultData();
+        try {
+            List<Express> list = sqlSession.selectList("selling.express.query", condition);
+            result.setData(list);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription(e.getMessage());
+        } finally {
+            return result;
+        }
+    }
 }
