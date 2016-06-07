3d3fdac1d4ece3da3f0da996bdf7aac5fac6309e
zhangxufan <sunshinezxf@hotmail.com>
2016-06-07 13:53:54 +0800

    add interface

--- src/main/java/selling/sunshine/controller/WithdrawController.java
+++ src/main/java/selling/sunshine/controller/WithdrawController.java
@@ -1,7 +1,22 @@
 package selling.sunshine.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by sunshine on 6/7/16.
  */
+@RestController
+@RequestMapping("/withdraw")
 public class WithdrawController {
+    private Logger logger = LoggerFactory.getLogger(WithdrawController.class);
+
+    public ModelAndView check() {
+        ModelAndView view = new ModelAndView();
+        
+        return view;
+    }
 }
