963638618ffa0a554c07792b7f1c8cf0265f9ca6
zhangxufan <sunshinezxf@hotmail.com>
2016-06-07 14:29:46 +0800

    modification

--- src/main/java/selling/sunshine/controller/WithdrawController.java
+++ src/main/java/selling/sunshine/controller/WithdrawController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import selling.sunshine.service.AgentService;
-import selling.sunshine.utils.ResultData;
 
 import java.util.HashMap;
 import java.util.Map;
