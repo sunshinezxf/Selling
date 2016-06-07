ac92c68881300938efe1adcb36bdbf315a2dfcb1
zhangxufan <sunshinezxf@hotmail.com>
2016-06-07 16:53:12 +0800

    modify preferred

--- src/main/java/selling/sunshine/model/BankCard.java
+++ src/main/java/selling/sunshine/model/BankCard.java
@@ -1,46 +1,53 @@
 package selling.sunshine.model;
 
-public class BankCard extends Entity{
-	private String bankCardId;
-	private String bankCardNo;
-	private Agent agent;
-	private int preferred;
-	
-	public BankCard(){
-		super();
-		this.preferred = 1;
-	}
-	
-	public BankCard(String bankCardNo, Agent agent){
-		this();
-		this.bankCardNo = bankCardNo;
-		this.agent = agent;
-	}
-	
-	public String getBankCardId() {
-		return bankCardId;
-	}
-	public void setBankCardId(String bankCardId) {
-		this.bankCardId = bankCardId;
-	}
-	public String getBankCardNo() {
-		return bankCardNo;
-	}
-	public void setBankCardNo(String bankCardNo) {
-		this.bankCardNo = bankCardNo;
-	}
-	public Agent getAgent() {
-		return agent;
-	}
-	public void setAgent(Agent agent) {
-		this.agent = agent;
-	}
-	public int getPreferred() {
-		return preferred;
-	}
-	public void setPreferred(int preferred) {
-		this.preferred = preferred;
-	}
-	
-	
+public class BankCard extends Entity {
+    private String bankCardId;
+    private String bankCardNo;
+    private Agent agent;
+    private boolean preferred;
+
+    public BankCard() {
+        super();
+        preferred = true;
+    }
+
+    public BankCard(String bankCardNo, Agent agent) {
+        this();
+        this.bankCardNo = bankCardNo;
+        this.agent = agent;
+    }
+
+    public String getBankCardId() {
+        return bankCardId;
+    }
+
+    public void setBankCardId(String bankCardId) {
+        this.bankCardId = bankCardId;
+    }
+
+    public String getBankCardNo() {
+        return bankCardNo;
+    }
+
+    public void setBankCardNo(String bankCardNo) {
+        this.bankCardNo = bankCardNo;
+    }
+
+    public Agent getAgent() {
+        return agent;
+    }
+
+    public void setAgent(Agent agent) {
+        this.agent = agent;
+    }
+
+    public boolean getPreferred() {
+        return preferred;
+    }
+
+    public void setPreferred(boolean preferred) {
+        this.preferred = preferred;
+    }
+
+
 }
