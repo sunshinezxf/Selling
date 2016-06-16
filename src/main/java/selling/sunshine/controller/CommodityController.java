abd1baeaaef722c939c2e5815d4d9e3b1baa6375
zhangxufan <sunshinezxf@hotmail.com>
2016-06-16 14:33:50 +0800

    modification

--- src/main/java/selling/sunshine/controller/CommodityController.java
+++ src/main/java/selling/sunshine/controller/CommodityController.java
@@ -1,5 +1,6 @@
 package selling.sunshine.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
