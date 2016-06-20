814348e7ac6da2b160edf2022e06a3c75965509b
zhangxufan <sunshinezxf@hotmail.com>
2016-06-20 13:59:20 +0800

    mobile page method for customer order

--- src/main/java/selling/sunshine/dao/CustomerOrderDao.java
+++ src/main/java/selling/sunshine/dao/CustomerOrderDao.java
@@ -1,17 +1,20 @@
 package selling.sunshine.dao;
 
-import java.util.Map;
-
 import selling.sunshine.model.CustomerOrder;
+import selling.sunshine.pagination.MobilePageParam;
 import selling.sunshine.utils.ResultData;
+
+import java.util.Map;
 
 /**
  * Created by sunshine on 6/14/16.
  */
 public interface CustomerOrderDao {
     ResultData insertOrder(CustomerOrder order);
-    
+
     ResultData updateOrder(CustomerOrder order);
-    
+
     ResultData queryOrder(Map<String, Object> condition);
+
+    ResultData queryOrder(Map<String, Object> condition, MobilePageParam param);
 }
