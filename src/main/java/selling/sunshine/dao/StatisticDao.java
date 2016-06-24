f3dddaeaabdfa677012e0023e170fac560e80cf4
zhangxufan <sunshinezxf@hotmail.com>
2016-06-25 01:51:03 +0800

    implementation of graphic in order check page

---
+++ src/main/java/selling/sunshine/dao/StatisticDao.java
@@ -0,0 +1,10 @@
+package selling.sunshine.dao;
+
+import selling.sunshine.utils.ResultData;
+
+/**
+ * Created by sunshine on 6/24/16.
+ */
+public interface StatisticDao {
+    ResultData queryOrderSum();
+}
