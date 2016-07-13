1a58f70cc7587f08cc2b780b744eacb612406c61
zhangxufan <sunshinezxf@hotmail.com>
2016-07-13 17:48:03 +0800

    modify template in deliver template

--- src/main/java/selling/sunshine/utils/WorkBookUtil.java
+++ src/main/java/selling/sunshine/utils/WorkBookUtil.java
@@ -34,6 +34,24 @@ public class WorkBookUtil {
         return workbook;
     }
 
+    public static Workbook getDeliverTemplate() {
+        Workbook workbook = null;
+        String path = WorkBookUtil.class.getResource("/").getPath();
+        int index = path.lastIndexOf("/WEB-INF/classes/");
+        String parent = path.substring(0, index);
+        File file = new File(parent + PlatformConfig.getValue("deliver_template"));
+        if (!file.exists()) {
+            logger.error("订货单模板文件不存在");
+        }
+        try {
+            OPCPackage pkg = OPCPackage.open(file);
+            workbook = new XSSFWorkbook(pkg);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return workbook;
+    }
+
     public static boolean createIndentTemplate(String context) {
         int length = 7;
         try {
