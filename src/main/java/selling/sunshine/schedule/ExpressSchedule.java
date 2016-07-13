f55b16e07f97155119dc03bee152c012707186f3
zhangxufan <sunshinezxf@hotmail.com>
2016-07-13 16:04:05 +0800

    add new schedule

---
+++ src/main/java/selling/sunshine/schedule/ExpressSchedule.java
@@ -0,0 +1,20 @@
+package selling.sunshine.schedule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import selling.sunshine.service.ExpressService;
+
+/**
+ * Created by sunshine on 7/13/16.
+ */
+public class ExpressSchedule {
+    private Logger logger = LoggerFactory.getLogger(ExpressSchedule.class);
+
+    @Autowired
+    private ExpressService expressService;
+
+    public void schedule() {
+        
+    }
+}
