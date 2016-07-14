0f0d94359d648285673b6a52346598b2b50da13d
zhangxufan <sunshinezxf@hotmail.com>
2016-07-14 13:27:55 +0800

    add message controller

--- src/main/java/selling/sunshine/controller/MessageController.java
+++ src/main/java/selling/sunshine/controller/MessageController.java
@@ -3,7 +3,9 @@ package selling.sunshine.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import selling.sunshine.utils.ResultData;
 
 /**
@@ -14,7 +16,15 @@ import selling.sunshine.utils.ResultData;
 public class MessageController {
     private Logger logger = LoggerFactory.getLogger(MessageController.class);
 
-    public ResultData message() {
+    @RequestMapping(method = RequestMethod.GET, value = "/send")
+    public ModelAndView message() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("/backend/message/send");
+        return view;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/send")
+    public ResultData message(String text) {
         ResultData result = new ResultData();
 
         return result;
