b870f984406f193a748fa578048c05c919709c23
zhangxufan <sunshinezxf@hotmail.com>
2016-08-03 17:09:12 +0800

    add orderr status

---
+++ src/main/java/selling/sunshine/controller/ReimburseController.java
@@ -0,0 +1,13 @@
+package selling.sunshine.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sunshine on 8/3/16.
+ */
+@RestController
+public class ReimburseController {
+    private Logger logger = LoggerFactory.getLogger(ReimburseController.class);
+}
