2131973386d9aab9f24af1b624bd834cce9df419
zhangxufan <sunshinezxf@hotmail.com>
2016-08-10 10:33:07 +0800

    add cash back level

---
+++ src/main/java/selling/sunshine/model/cashback/CashBackLevel.java
@@ -0,0 +1,19 @@
+package selling.sunshine.model.cashback;
+
+/**
+ * Created by sunshine on 8/10/16.
+ */
+public enum CashBackLevel {
+
+    SELF(0), DIRECT(1), INDIRECT(2);
+
+    private int code;
+
+    CashBackLevel(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+}
