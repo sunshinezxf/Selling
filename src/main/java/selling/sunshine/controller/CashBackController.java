3d1e9b8236f0a8cccf1230cabc43acaea09bf306
zhangxufan <sunshinezxf@hotmail.com>
2016-08-12 10:37:42 +0800

    implementation of cashback month overview

---
+++ src/main/java/selling/sunshine/controller/CashBackController.java
@@ -0,0 +1,7 @@
+package selling.sunshine.controller;
+
+/**
+ * Created by sunshine on 8/12/16.
+ */
+public class CashBackController {
+}
---
+++ src/main/java/selling/sunshine/service/impl/CashBackServiceImpl.java
@@ -0,0 +1,7 @@
+package selling.sunshine.service.impl;
+
+/**
+ * Created by sunshine on 8/12/16.
+ */
+public class CashBackServiceImpl {
+}
