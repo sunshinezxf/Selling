99729a84b7ca61d9f8d51c1375fa4f59f00a674f
zhangxufan <sunshinezxf@hotmail.com>
2016-08-12 10:39:10 +0800

    modifications

--- src/main/java/selling/sunshine/service/CashBackService.java
+++ src/main/java/selling/sunshine/service/CashBackService.java
@@ -1,7 +1,13 @@
 package selling.sunshine.service;
 
+import selling.sunshine.pagination.DataTableParam;
+import selling.sunshine.utils.ResultData;
+
+import java.util.Map;
+
 /**
  * Created by sunshine on 8/12/16.
  */
 public interface CashBackService {
+    ResultData fetchCashBackPerMonth(Map<String, Object> condition, DataTableParam param);
 }
--- src/main/java/selling/sunshine/service/impl/CashBackServiceImpl.java
+++ src/main/java/selling/sunshine/service/impl/CashBackServiceImpl.java
@@ -1,7 +1,40 @@
 package selling.sunshine.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import selling.sunshine.dao.CashBackDao;
+import selling.sunshine.pagination.DataTableParam;
+import selling.sunshine.service.CashBackService;
+import selling.sunshine.utils.ResponseCode;
+import selling.sunshine.utils.ResultData;
+import selling.sunshine.vo.cashback.CashBack4AgentPerMonth;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by sunshine on 8/12/16.
  */
-public class CashBackServiceImpl {
+@Service
+public class CashBackServiceImpl implements CashBackService {
+    private Logger logger = LoggerFactory.getLogger(CashBackServiceImpl.class);
+
+    @Autowired
+    private CashBackDao cashBackDao;
+
+    @Override
+    public ResultData fetchCashBackPerMonth(Map<String, Object> condition, DataTableParam param) {
+        ResultData result = new ResultData();
+        ResultData response = cashBackDao.queryMonthlyByPage(condition, param);
+        result.setResponseCode(response.getResponseCode());
+        if (response.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            if (((List<CashBack4AgentPerMonth>) response.getData()).isEmpty()) {
+                result.setResponseCode(ResponseCode.RESPONSE_NULL);
+            }
+            result.setData(response.getData());
+        }
+        return result;
+    }
 }
