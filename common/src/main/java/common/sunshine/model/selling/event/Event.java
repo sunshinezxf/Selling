76a8f541b45fdd36315944265a8f2596d0fa6c4a
zhangxufan <sunshinezxf@hotmail.com>
2016-08-23 23:45:00 +0800

    add primary framework for event

--- common/src/main/java/common/sunshine/model/selling/event/Event.java
+++ common/src/main/java/common/sunshine/model/selling/event/Event.java
@@ -5,11 +5,10 @@ import common.sunshine.model.Entity;
 import java.util.Calendar;
 
 /**
- * 赠送活动
  * 活动默认从当前时间开始,默认持续时间为7天
  * Created by sunshine on 8/23/16.
  */
-public class Event extends Entity {
+public abstract class Event extends Entity {
     private final int DURATION = 7;
 
     private String eventId;
@@ -24,6 +23,16 @@ public class Event extends Entity {
         this.start = Calendar.getInstance();
         this.end = Calendar.getInstance();
         this.end.add(Calendar.DATE, DURATION);
+    }
+
+    public Event(Calendar start, Calendar end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Event(String title, Calendar start, Calendar end) {
+        this(start, end);
+        this.title = title;
     }
 
     public String getEventId() {
---
+++ src/main/java/selling/sunshine/controller/EventController.java
@@ -0,0 +1,45 @@
+package selling.sunshine.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by sunshine on 8/23/16.
+ */
+@RestController
+@RequestMapping("/event")
+public class EventController {
+    private Logger logger = LoggerFactory.getLogger(EventController.class);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/create")
+    public ModelAndView create() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("backend/event/create");
+        return view;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/overview")
+    public ModelAndView overview() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("backend/event/overview");
+        return view;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{eventId}")
+    public ModelAndView preview(@PathVariable("eventId") String eventId) {
+        ModelAndView view = new ModelAndView();
+        return view;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{eventName}/")
+    public ModelAndView view(@PathVariable("eventName") String eventName) {
+        ModelAndView view = new ModelAndView();
+
+        return view;
+    }
+}
