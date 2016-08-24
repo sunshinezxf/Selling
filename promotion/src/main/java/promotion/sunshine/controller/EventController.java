d64504040eb36e7d4b4e2b3c09c94435cb2ab2db
unknown <>
2016-08-24 16:03:41 +0800

    1

--- promotion/src/main/java/promotion/sunshine/controller/EventController.java
+++ promotion/src/main/java/promotion/sunshine/controller/EventController.java
@@ -2,11 +2,14 @@ package promotion.sunshine.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import promotion.sunshine.service.EventService;
 
 /**
  * Created by sunshine on 8/22/16.
@@ -15,6 +18,9 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/event")
 public class EventController {
     private Logger logger = LoggerFactory.getLogger(EventController.class);
+    
+    @Autowired
+    private EventService eventService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/{eventName}/")
     public ModelAndView view(@PathVariable("eventName") String eventName) {
