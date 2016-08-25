853700d29cbde4c17c5e55057306324a561be8eb
unknown <>
2016-08-25 10:54:23 +0800

    1

--- promotion/src/main/java/promotion/sunshine/controller/EventController.java
+++ promotion/src/main/java/promotion/sunshine/controller/EventController.java
@@ -63,6 +63,12 @@ public class EventController {
     public ModelAndView giftApplication(@Valid EventForm form, BindingResult result){
     	ModelAndView view = new ModelAndView();
     	EventApplication eventApplication = new EventApplication(form.getDonor_name(),form.getDonor_phone(),form.getDonee_name(),form.getDonee_phone(),form.getDonee_gender(),form.getDonee_address(),form.getRelation(),form.getWishes());
+    	ResultData insertEventApplicationResponse = eventService.insertEventApplication(eventApplication);
+    	if(insertEventApplicationResponse.getResponseCode() != ResponseCode.RESPONSE_OK){
+    		//错误页面
+    		return view;
+    	}
+    	//正常Prompt页面
     	return view;
     }
 }
