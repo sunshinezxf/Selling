872a9dcc5ba8c3ce7e7a04fbab1158dc454de608
unknown <>
2016-09-02 01:11:15 +0800

    1

---
+++ promotion/src/main/java/promotion/sunshine/schedule/WechatSchedule.java
@@ -0,0 +1,18 @@
+package promotion.sunshine.schedule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import promotion.sunshine.utils.PlatformConfig;
+import promotion.sunshine.utils.WechatUtil;
+
+/**
+ * Created by sunshine on 5/24/16.
+ */
+public class WechatSchedule {
+    private Logger logger = LoggerFactory.getLogger(WechatSchedule.class);
+
+    public void schedule() {
+        PlatformConfig.setAccessToken(WechatUtil.queryAccessToken());
+    }
+}
