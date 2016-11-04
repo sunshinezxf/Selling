2f1dda0396fecb5a2eccd2b11cb638e9a3c3a75e
wxd <wxd11@gmail.com>
2016-11-04 11:03:06 +0800

    Changes to be committed: 	new file:   common/src/main/java/common/sunshine/model/selling/agent/AgentKPI.java

---
+++ common/src/main/java/common/sunshine/model/selling/agent/AgentKPI.java
@@ -0,0 +1,72 @@
+package common.sunshine.model.selling.agent;
+
+import common.sunshine.model.Entity;
+
+public class AgentKPI extends Entity {
+	private String kpiId;
+	private String agentId;
+	private String agentName;
+	private int customerQuantity;
+	private int directAgentQuantity;
+	private int agentContribution;
+
+	public AgentKPI(String kpiId, String agentId, String agentName, int customerQuantity, int directAgentQuantity,
+			int agentContribution) {
+		super();
+		this.kpiId = kpiId;
+		this.agentId = agentId;
+		this.agentName = agentName;
+		this.customerQuantity = customerQuantity;
+		this.directAgentQuantity = directAgentQuantity;
+		this.agentContribution = agentContribution;
+	}
+
+	public String getKpiId() {
+		return kpiId;
+	}
+
+	public void setKpiId(String kpiId) {
+		this.kpiId = kpiId;
+	}
+
+	public String getAgentId() {
+		return agentId;
+	}
+
+	public void setAgentId(String agentId) {
+		this.agentId = agentId;
+	}
+
+	public String getAgentName() {
+		return agentName;
+	}
+
+	public void setAgentName(String agentName) {
+		this.agentName = agentName;
+	}
+
+	public int getCustomerQuantity() {
+		return customerQuantity;
+	}
+
+	public void setCustomerQuantity(int customerQuantity) {
+		this.customerQuantity = customerQuantity;
+	}
+
+	public int getDirectAgentQuantity() {
+		return directAgentQuantity;
+	}
+
+	public void setDirectAgentQuantity(int directAgentQuantity) {
+		this.directAgentQuantity = directAgentQuantity;
+	}
+
+	public int getAgentContribution() {
+		return agentContribution;
+	}
+
+	public void setAgentContribution(int agentContribution) {
+		this.agentContribution = agentContribution;
+	}
+
+}
