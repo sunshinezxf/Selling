e3d95262b372ec2aff068a69ddf04b3d30ab9e5a
zhangxufan <sunshinezxf@hotmail.com>
2016-11-14 09:56:26 +0800

    add interface of sales volume query

--- src/main/java/selling/sunshine/controller/StatisticController.java
+++ src/main/java/selling/sunshine/controller/StatisticController.java
@@ -1,31 +1,25 @@
 package selling.sunshine.controller;
 
 import com.alibaba.fastjson.JSONArray;
-
 import com.alibaba.fastjson.JSONObject;
-
+import common.sunshine.model.selling.customer.Customer;
+import common.sunshine.pagination.DataTablePage;
+import common.sunshine.pagination.DataTableParam;
+import common.sunshine.utils.ResponseCode;
+import common.sunshine.utils.ResultData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import selling.sunshine.model.sum.AgentGoods;
 import selling.sunshine.model.sum.OrderSeries;
 import selling.sunshine.model.sum.OrderStatistics;
-import common.sunshine.model.selling.customer.Customer;
-import common.sunshine.pagination.DataTablePage;
-import common.sunshine.pagination.DataTableParam;
 import selling.sunshine.service.AgentService;
 import selling.sunshine.service.CustomerService;
 import selling.sunshine.service.OrderService;
 import selling.sunshine.service.StatisticService;
 import selling.sunshine.utils.BaiduMapUtils;
-import common.sunshine.utils.ResponseCode;
-import common.sunshine.utils.ResultData;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -50,7 +44,7 @@ public class StatisticController {
 
     @Autowired
     private StatisticService statisticService;
-    
+
     @Autowired
     private CustomerService customerService;
 
@@ -169,139 +163,153 @@ public class StatisticController {
         logger.debug("data sum: " + result);
         return result;
     }
-    
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/order/month")
     public DataTablePage<OrderStatistics> orderMonth(DataTableParam param) {
-    	 DataTablePage<OrderStatistics> result = new DataTablePage<>();
-         if (StringUtils.isEmpty(param)) {
-             return result;
-         }
-         ResultData fetchResponse = statisticService.orderStatisticsByPage(param);
-         if (fetchResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
-             result = (DataTablePage<OrderStatistics>) fetchResponse.getData();
-         }
-         return result;
-    } 
-    
+        DataTablePage<OrderStatistics> result = new DataTablePage<>();
+        if (StringUtils.isEmpty(param)) {
+            return result;
+        }
+        ResultData fetchResponse = statisticService.orderStatisticsByPage(param);
+        if (fetchResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            result = (DataTablePage<OrderStatistics>) fetchResponse.getData();
+        }
+        return result;
+    }
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/agent/goods/month")
     public DataTablePage<AgentGoods> agentGoodsMonth(DataTableParam param) {
-    	 DataTablePage<AgentGoods> result = new DataTablePage<>();
-         if (StringUtils.isEmpty(param)) {
-             return result;
-         }
-         ResultData fetchResponse = statisticService.agentGoodsMonthByPage(param);
-         if (fetchResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
-             result = (DataTablePage<AgentGoods>) fetchResponse.getData();
-         }
-         return result;
-    } 
-    
+        DataTablePage<AgentGoods> result = new DataTablePage<>();
+        if (StringUtils.isEmpty(param)) {
+            return result;
+        }
+        ResultData fetchResponse = statisticService.agentGoodsMonthByPage(param);
+        if (fetchResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            result = (DataTablePage<AgentGoods>) fetchResponse.getData();
+        }
+        return result;
+    }
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/agent/goods")
     public DataTablePage<AgentGoods> agentGoods(DataTableParam param) {
-    	 DataTablePage<AgentGoods> result = new DataTablePage<>();
-         if (StringUtils.isEmpty(param)) {
-             return result;
-         }
-         ResultData fetchResponse = statisticService.agentGoodsByPage(param);
-         if (fetchResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
-             result = (DataTablePage<AgentGoods>) fetchResponse.getData();
-         }
-         return result;
-    } 
-    
+        DataTablePage<AgentGoods> result = new DataTablePage<>();
+        if (StringUtils.isEmpty(param)) {
+            return result;
+        }
+        ResultData fetchResponse = statisticService.agentGoodsByPage(param);
+        if (fetchResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            result = (DataTablePage<AgentGoods>) fetchResponse.getData();
+        }
+        return result;
+    }
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/orderByYear")
     public JSONObject orderByYear() {
-    	JSONObject result = new JSONObject();
-        ResultData resultData=statisticService.orderByYear();
-        if (resultData.getResponseCode()==ResponseCode.RESPONSE_OK) {
-			List<OrderSeries> list=(List<OrderSeries>)resultData.getData();
-			JSONArray series=new JSONArray();
-			for (int i = 0; i < list.size(); i++) {
-				JSONArray data=new JSONArray();
-				for (int j = 0; j < list.get(i).getData().length; j++) {
-					data.add(list.get(i).getData()[j]);
-				}
-				JSONObject jsonObject=new JSONObject();
-				jsonObject.put("name", list.get(i).getName());
-				jsonObject.put("data", data);
-				series.add(jsonObject);
-			}
-			result.put("series", series); 	
-		}
+        JSONObject result = new JSONObject();
+        ResultData resultData = statisticService.orderByYear();
+        if (resultData.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            List<OrderSeries> list = (List<OrderSeries>) resultData.getData();
+            JSONArray series = new JSONArray();
+            for (int i = 0; i < list.size(); i++) {
+                JSONArray data = new JSONArray();
+                for (int j = 0; j < list.get(i).getData().length; j++) {
+                    data.add(list.get(i).getData()[j]);
+                }
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("name", list.get(i).getName());
+                jsonObject.put("data", data);
+                series.add(jsonObject);
+            }
+            result.put("series", series);
+        }
         return result;
     }
-    
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/purchaseRecordEveryday")
-    public JSONObject purchaseRecordEveryday(){
-    	JSONObject result = new JSONObject();
-        ResultData resultData=statisticService.purchaseRecordEveryday();
-        result=(JSONObject)resultData.getData();
+    public JSONObject purchaseRecordEveryday() {
+        JSONObject result = new JSONObject();
+        ResultData resultData = statisticService.purchaseRecordEveryday();
+        result = (JSONObject) resultData.getData();
         return result;
     }
-    
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/purchaseRecordEveryMonth")
-    public JSONObject purchaseRecordEveryMonth(){
-    	JSONObject result = new JSONObject();
-        ResultData resultData=statisticService.purchaseRecordEveryMonth();
-        result=(JSONObject)resultData.getData();
+    public JSONObject purchaseRecordEveryMonth() {
+        JSONObject result = new JSONObject();
+        ResultData resultData = statisticService.purchaseRecordEveryMonth();
+        result = (JSONObject) resultData.getData();
         return result;
     }
-    
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/purchaseRecordEveryday2")
-    public JSONObject purchaseRecordEveryday2(){
-    	JSONObject result = new JSONObject();
-        ResultData resultData=statisticService.purchaseRecordEveryday2();
-        result=(JSONObject)resultData.getData();
+    public JSONObject purchaseRecordEveryday2() {
+        JSONObject result = new JSONObject();
+        ResultData resultData = statisticService.purchaseRecordEveryday2();
+        result = (JSONObject) resultData.getData();
         return result;
     }
-    
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/purchaseRecordEveryMonth2")
-    public JSONObject purchaseRecordEveryMonth2(){
-    	JSONObject result = new JSONObject();
-        ResultData resultData=statisticService.purchaseRecordEveryMonth2();
-        result=(JSONObject)resultData.getData();
+    public JSONObject purchaseRecordEveryMonth2() {
+        JSONObject result = new JSONObject();
+        ResultData resultData = statisticService.purchaseRecordEveryMonth2();
+        result = (JSONObject) resultData.getData();
         return result;
     }
-    
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET, value = "/sales/area")
     public ResultData salesArea() throws IOException {
-    	ResultData resultData=new ResultData();
-    	Map<String, Object> condition=new HashMap<>();
-    	condition.put("blockFlag", false);
-    	ResultData queryData=customerService.fetchCustomer(condition);
-    	if (queryData.getResponseCode()==ResponseCode.RESPONSE_OK) {
-    		List<Customer> customers=(List<Customer>)queryData.getData();
-    		Map<String, Integer> map=new HashMap<>();
-    		for (Customer customer:customers) {
-				Map<String, BigDecimal> map1 = BaiduMapUtils.getLatAndLngByAddress(customer.getAddress().getAddress());
-				BigDecimal lat=map1.get("lat");
-				BigDecimal lng=map1.get("lng");
-				Map<String, String> map2=BaiduMapUtils.getAddressByLatAndLng(String.valueOf(lat), String.valueOf(lng));
-				String province=map2.get("province");
-				if (map.containsKey(province)) {
-					map.put(province, map.get(province)+1);
-				}else {
-					map.put(province, 1);
-				}
-			}
-    		resultData.setData(map);
-		}
-    	return resultData;
-	}
-    
+        ResultData resultData = new ResultData();
+        Map<String, Object> condition = new HashMap<>();
+        condition.put("blockFlag", false);
+        ResultData queryData = customerService.fetchCustomer(condition);
+        if (queryData.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            List<Customer> customers = (List<Customer>) queryData.getData();
+            Map<String, Integer> map = new HashMap<>();
+            for (Customer customer : customers) {
+                Map<String, BigDecimal> map1 = BaiduMapUtils.getLatAndLngByAddress(customer.getAddress().getAddress());
+                BigDecimal lat = map1.get("lat");
+                BigDecimal lng = map1.get("lng");
+                Map<String, String> map2 = BaiduMapUtils.getAddressByLatAndLng(String.valueOf(lat), String.valueOf(lng));
+                String province = map2.get("province");
+                if (map.containsKey(province)) {
+                    map.put(province, map.get(province) + 1);
+                } else {
+                    map.put(province, 1);
+                }
+            }
+            resultData.setData(map);
+        }
+        return resultData;
+    }
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET, value = "/agent/ranking")
-    public ResultData agentRanking(){
-    	ResultData resultData=new ResultData();
-    	return resultData;
+    public ResultData agentRanking() {
+        ResultData resultData = new ResultData();
+        return resultData;
+    }
+
+    /**
+     * @param type, type的取值daily, monthly, overall
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/sales/volume/{type}")
+    public ResultData sales(@PathVariable("type") String type) {
+        ResultData result = new ResultData();
+        Map<String, Object> condition = new HashMap<>();
+        condition.put("filter", type);
+
+        return result;
     }
 }
