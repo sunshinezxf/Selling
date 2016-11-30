fa66e2dc5204118757c1b1beb878e3ba80dc3e7f
unknown <>
2016-11-30 13:06:51 +0800

    1

--- src/main/java/selling/sunshine/controller/StatementController.java
+++ src/main/java/selling/sunshine/controller/StatementController.java
@@ -1,9 +1,18 @@
 package selling.sunshine.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
 import selling.sunshine.service.StatementService;
 
 public class StatementController {
@@ -13,5 +22,8 @@ public class StatementController {
     @Autowired
     private StatementService statementService;
 
-
+    @RequestMapping(method = RequestMethod.GET, value = "/downloadGiftExcel")
+    public String downloadGiftExcel(HttpServletRequest request, HttpServletResponse response) throws IOException, RowsExceededException, WriteException {
+    	return null;
+    }
 }
