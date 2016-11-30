cfbeae0550dfab3649576e962dc349782a00f047
wxd <wxd11@gmail.com>
2016-11-30 12:58:42 +0800

    Changes to be committed: 	modified:   src/main/java/selling/sunshine/service/StatementService.java 	modified:   src/main/java/selling/sunshine/service/impl/StatementServiceImpl.java

--- src/main/java/selling/sunshine/service/StatementService.java
+++ src/main/java/selling/sunshine/service/StatementService.java
@@ -1,5 +1,10 @@
 package selling.sunshine.service;
 
+import common.sunshine.utils.ResultData;
+
 public interface StatementService {
+	
+	//活动统计报表信息
+	ResultData eventStatistic();
 
 }
--- src/main/java/selling/sunshine/service/impl/StatementServiceImpl.java
+++ src/main/java/selling/sunshine/service/impl/StatementServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import common.sunshine.utils.ResultData;
 import selling.sunshine.dao.StatementDao;
 import selling.sunshine.service.StatementService;
 
@@ -13,6 +14,12 @@ public class StatementServiceImpl implements StatementService {
     
     @Autowired
     private StatementDao statementDao;
+
+	@Override
+	public ResultData eventStatistic() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 
 }
