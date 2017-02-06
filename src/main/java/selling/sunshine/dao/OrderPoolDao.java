fb0e14b533f731ad1c694431004c34442b47e9af
wxd <wxd11@gmail.com>
2017-02-06 13:17:26 +0800

    Changes to be committed: 	modified:   src/main/java/selling/sunshine/dao/OrderPoolDao.java 	modified:   src/main/java/selling/sunshine/dao/impl/OrderDaoImpl.java 	modified:   src/main/java/selling/sunshine/dao/impl/OrderPoolDaoImpl.java 	modified:   src/main/java/selling/sunshine/dao/impl/RefundDaoImpl.java 	modified:   src/main/java/selling/sunshine/mapper/customerorder.xml 	modified:   src/main/java/selling/sunshine/mapper/orderpool.xml

--- src/main/java/selling/sunshine/dao/OrderPoolDao.java
+++ src/main/java/selling/sunshine/dao/OrderPoolDao.java
@@ -1,11 +1,12 @@
 package selling.sunshine.dao;
 
-import java.util.Map;
-
 import common.sunshine.utils.ResultData;
+
+import java.util.Map;
 
 public interface OrderPoolDao {
 	ResultData queryOrderPool(Map<String, Object> condition);
 	
 	ResultData checkOrderPool(Map<String, Object> condition);
+
 }
