71816e48e71241d66da757414cb60dc0b8a05a9a
wxd <wxd11@gmail.com>
2017-02-08 14:36:32 +0800

    modified:   common/src/main/java/common/sunshine/model/wechat/Article.java

--- common/src/main/java/common/sunshine/model/wechat/Article.java
+++ common/src/main/java/common/sunshine/model/wechat/Article.java
@@ -2,6 +2,8 @@ package common.sunshine.model.wechat;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
+import java.util.Objects;
+
 /**
  * Created by sunshine on 7/11/16.
  */
@@ -50,4 +52,27 @@ public class Article {
     public void setUrl(String url) {
         Url = url;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        // 如果为同一对象的不同引用,则相同
+        if (this == obj) {
+            return true;
+        }
+        // 如果传入的对象为空,则返回false
+        if (obj == null) {
+            return false;
+        }
+
+        // 如果两者属于不同的类型,不能相等
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        // 类型相同, 比较内容是否相同
+        Article other = (Article) obj;
+
+        return Objects.equals(Title, other.Title) && Objects.equals(Description, other.Description) && Objects.equals(PicUrl, other.PicUrl) && Objects.equals(Url, other.Url);
+    }
+
 }
