3cd12d14ccfcf91fa45700af6f84d116f506b45c
zhangxufan <sunshinezxf@hotmail.com>
2017-02-27 16:22:23 +0800

    modify comment for httputil

--- src/main/java/selling/sunshine/utils/HttpUtil.java
+++ src/main/java/selling/sunshine/utils/HttpUtil.java
@@ -1,4 +1,5 @@
 package selling.sunshine.utils;
+
 import com.alibaba.fastjson.JSONObject;
 
 import java.io.*;
@@ -9,51 +10,53 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 /**
+ * 该类为中通物流所使用
+ *
  * @author sunshine
  */
 public class HttpUtil {
-	
-	/**
-	 * 发送POST 请求
-	 * @param url 请求地址
-	 * @param charset 编码格式
-	 * @param params 请求参数
-	 * @return 响应
-	 * @throws IOException
-	 */
-	public static String post(String url, String charset, Map params) throws IOException {
-		HttpURLConnection conn = null;
-		OutputStreamWriter out = null;
-		InputStream inputStream = null;
+
+    /**
+     * 发送POST 请求
+     *
+     * @param {String} url     请求地址
+     * @param {String} charset 编码格式
+     * @param {Map}    params  请求参数
+     * @return 响应消息
+     * @throws IOException
+     */
+    public static String post(String url, String charset, Map params) throws IOException {
+        HttpURLConnection conn;
+        OutputStreamWriter out = null;
+        InputStream inputStream = null;
         InputStreamReader inputStreamReader = null;
         BufferedReader reader = null;
         StringBuffer result = new StringBuffer();
-		try {
-			conn = (HttpURLConnection)new URL(url).openConnection();
-			conn.setDoOutput(true);
-			conn.setDoInput(true);
-			conn.setRequestMethod("POST"); 
-			conn.setRequestProperty("accept", "*/*");
-			conn.setRequestProperty("connection", "Keep-Alive");
-			conn.setRequestProperty("Accept-Charset", charset);
-			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			out = new OutputStreamWriter(conn.getOutputStream(), charset);
-			out.write(buildQuery(params, charset));
-			out.flush();
-			inputStream = conn.getInputStream();
-			inputStreamReader = new InputStreamReader(inputStream);
+        try {
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("Accept-Charset", charset);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            out = new OutputStreamWriter(conn.getOutputStream(), charset);
+            out.write(buildQuery(params, charset));
+            out.flush();
+            inputStream = conn.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream);
             reader = new BufferedReader(inputStreamReader);
-            String tempLine = null;
+            String tempLine;
             while ((tempLine = reader.readLine()) != null) {
-            	result.append(tempLine);
+                result.append(tempLine);
             }
-            
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (out != null) {
-				out.close();
-			}
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null) {
+                out.close();
+            }
             if (reader != null) {
                 reader.close();
             }
@@ -63,52 +66,53 @@ public class HttpUtil {
             if (inputStream != null) {
                 inputStream.close();
             }
-		}
-		return result.toString();
-	}
+        }
+        return result.toString();
+    }
 
 
-	/**
-	 * post请求，以json格式返回
-	 * @param url
-	 * @param charset
-	 * @param params
-	 * @return
-	 * @throws Exception
-	 */
-	public static String postJSON(String url, String charset, JSONObject params) throws Exception{
-		HttpURLConnection conn = null;
-		OutputStreamWriter out = null;
-		InputStream inputStream = null;
+    /**
+     * post请求，以json格式返回
+     *
+     * @param {String} url 请求地址
+     * @param {String} charset 字符集
+     * @param {Map}    params 请求参数
+     * @return 响应消息
+     * @throws Exception
+     */
+    public static String postJSON(String url, String charset, JSONObject params) throws Exception {
+        HttpURLConnection conn;
+        OutputStreamWriter out = null;
+        InputStream inputStream = null;
         InputStreamReader inputStreamReader = null;
         BufferedReader reader = null;
         StringBuffer result = new StringBuffer();
-		try {
-			conn = (HttpURLConnection)new URL(url).openConnection();
-			conn.setDoOutput(true);
-			conn.setDoInput(true);
-			conn.setRequestMethod("POST"); 
-			conn.setRequestProperty("accept", "*/*");
-			conn.setRequestProperty("connection", "Keep-Alive");
-			conn.setRequestProperty("Accept-Charset", charset);
-			conn.setRequestProperty("Content-Type", "application/json");
-			out = new OutputStreamWriter(conn.getOutputStream(), charset);
-			out.write(params.toJSONString());
-			out.flush();
-			inputStream = conn.getInputStream();
-			inputStreamReader = new InputStreamReader(inputStream);
+        try {
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("Accept-Charset", charset);
+            conn.setRequestProperty("Content-Type", "application/json");
+            out = new OutputStreamWriter(conn.getOutputStream(), charset);
+            out.write(params.toJSONString());
+            out.flush();
+            inputStream = conn.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream);
             reader = new BufferedReader(inputStreamReader);
-            String tempLine = null;
+            String tempLine;
             while ((tempLine = reader.readLine()) != null) {
-            	result.append(tempLine);
+                result.append(tempLine);
             }
-            
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (out != null) {
-				out.close();
-			}
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null) {
+                out.close();
+            }
             if (reader != null) {
                 reader.close();
             }
@@ -118,36 +122,35 @@ public class HttpUtil {
             if (inputStream != null) {
                 inputStream.close();
             }
-		}
-		return result.toString();
-	}
-	
-	/**
-	 * 将map转换为请求字符串
-	 * <p>data=xxx&msg_type=xxx</p>
-	 * @param params
-	 * @param charset
-	 * @return
-	 * @throws IOException
-	 */
-	public static String buildQuery(Map<String, Object> params, String charset) throws IOException {
-		if (params == null || params.isEmpty()) {
-			return null;
-		}
+        }
+        return result.toString();
+    }
 
-		StringBuffer data = new StringBuffer();
-		boolean flag = false;
+    /**
+     * 将map转换为请求字符串
+     *
+     * @param {Map}    params, 请求的参数信息
+     * @param {String} charset, 字符集
+     * @return 构建好的查询条件
+     * @throws IOException
+     */
+    public static String buildQuery(Map<String, Object> params, String charset) throws IOException {
+        if (params == null || params.isEmpty()) {
+            return null;
+        }
 
-		for (Entry<String, Object> entry : params.entrySet()) {
-			if (flag) {
-				data.append("&");
-			} else {
-				flag = true;
-			}
-			data.append(entry.getKey()).append("=").append(URLEncoder.encode(entry.getValue().toString(), charset));
-		}
-		System.out.println(data.toString());
-		return data.toString();
-	
-	}
+        StringBuffer data = new StringBuffer();
+        boolean flag = false;
+
+        for (Entry<String, Object> entry : params.entrySet()) {
+            if (flag) {
+                data.append("&");
+            } else {
+                flag = true;
+            }
+            data.append(entry.getKey()).append("=").append(URLEncoder.encode(entry.getValue().toString(), charset));
+        }
+        return data.toString();
+
+    }
 }
