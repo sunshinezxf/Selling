2c602b2d5164cd3728604612b59d7a1cc767b1f9
wxd <wxd11@gmail.com>
2017-05-02 15:13:34 +0800

    new file:   src/main/java/selling/sunshine/dao/impl/VouchersDaoImpl.java 	new file:   src/main/java/selling/sunshine/service/impl/VouchersServiceImpl.java

---
+++ src/main/java/selling/sunshine/dao/impl/VouchersDaoImpl.java
@@ -0,0 +1,74 @@
+package selling.sunshine.dao.impl;
+
+import common.sunshine.dao.BaseDao;
+import common.sunshine.model.selling.vouchers.Vouchers;
+import common.sunshine.utils.IDGenerator;
+import common.sunshine.utils.ResponseCode;
+import common.sunshine.utils.ResultData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import selling.sunshine.dao.VouchersDao;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wxd on 2017/5/2.
+ */
+public class VouchersDaoImpl extends BaseDao implements VouchersDao {
+
+    private Logger logger = LoggerFactory.getLogger(VouchersDaoImpl.class);
+
+    private Object lock = new Object();
+
+    @Override
+    public ResultData queryVouchers(Map<String, Object> condition) {
+        ResultData result = new ResultData();
+        try {
+            List<Vouchers> list = sqlSession.selectList("selling.vouchers.query", condition);
+            result.setData(list);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription(e.getMessage());
+        } finally {
+            return result;
+        }
+    }
+
+    @Override
+    public ResultData insertVouchers(Vouchers vouchers) {
+        ResultData result = new ResultData();
+        synchronized (lock) {
+            try {
+                vouchers.setVouchersId(IDGenerator.generate("VOU"));
+                sqlSession.insert("selling.vouchers.insert", vouchers);
+                result.setData(vouchers);
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+                result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+                result.setDescription(e.getMessage());
+            } finally {
+                return result;
+            }
+        }
+    }
+
+    @Override
+    public ResultData updateVouchers(Vouchers vouchers) {
+        ResultData result = new ResultData();
+        synchronized (lock) {
+            try {
+                sqlSession.update("selling.vouchers.update", vouchers);
+                result.setData(vouchers);
+            } catch (Exception e) {
+                logger.debug(e.getMessage());
+                result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+                result.setDescription(e.getMessage());
+            } finally {
+                return result;
+            }
+        }
+    }
+}
+
---
+++ src/main/java/selling/sunshine/service/impl/VouchersServiceImpl.java
@@ -0,0 +1,63 @@
+package selling.sunshine.service.impl;
+
+import common.sunshine.model.selling.vouchers.Vouchers;
+import common.sunshine.utils.ResponseCode;
+import common.sunshine.utils.ResultData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import selling.sunshine.dao.VouchersDao;
+import selling.sunshine.service.VouchersService;
+
+import java.util.Map;
+
+/**
+ * Created by wxd on 2017/5/2.
+ */
+public class VouchersServiceImpl implements VouchersService {
+
+    private Logger logger = LoggerFactory.getLogger(VouchersServiceImpl.class);
+
+    @Autowired
+    private VouchersDao vouchersDao;
+
+    @Override
+    public ResultData createVouchers(Vouchers vouchers) {
+        ResultData result = new ResultData();
+        ResultData insertResponse = vouchersDao.insertVouchers(vouchers);
+        result.setResponseCode(insertResponse.getResponseCode());
+        if (insertResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            result.setData(insertResponse.getData());
+        } else {
+            result.setDescription(insertResponse.getDescription());
+        }
+        return result;
+    }
+
+    @Override
+    public ResultData fetchVouchers(Map<String, Object> condition) {
+        ResultData result = new ResultData();
+        ResultData queryResponse = vouchersDao.queryVouchers(condition);
+        result.setResponseCode(queryResponse.getResponseCode());
+        if (queryResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            result.setData(queryResponse.getData());
+        } else if (queryResponse.getResponseCode() == ResponseCode.RESPONSE_ERROR) {
+            result.setDescription(queryResponse.getDescription());
+        }
+        return result;
+    }
+
+    @Override
+    public ResultData uesVouchers(Vouchers vouchers) {
+        ResultData result = new ResultData();
+        vouchers.setUsed(true);
+        ResultData updateResponse = vouchersDao.updateVouchers(vouchers);
+        result.setResponseCode(updateResponse.getResponseCode());
+        if (updateResponse.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            result.setData(updateResponse.getData());
+        } else {
+            result.setDescription(updateResponse.getDescription());
+        }
+        return result;
+    }
+}
