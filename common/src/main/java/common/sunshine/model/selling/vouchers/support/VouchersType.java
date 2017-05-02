9aa86c0d9936681bcb988a1ae2e71bf7f3d9eee9
wxd <wxd11@gmail.com>
2017-05-02 15:16:15 +0800

    modified:   common/src/main/java/common/sunshine/model/selling/vouchers/support/VouchersType.java

--- common/src/main/java/common/sunshine/model/selling/vouchers/support/VouchersType.java
+++ common/src/main/java/common/sunshine/model/selling/vouchers/support/VouchersType.java
@@ -7,7 +7,7 @@ package common.sunshine.model.selling.vouchers.support;
 public enum VouchersType {
 
     /*注册送的代金券，社群拓展奖励的代金券*/
-    RESGISTER(0),REWARD(1);
+    REGISTER(0),REWARD(1);
 
     private int code;
 
